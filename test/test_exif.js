88d63d648c3f5b01bff319be7b1973d5a2e31e00
James E. Marca <jmarca@translab.its.uci.edu>
2014-04-13 14:02:47 -0700

    extracting exif data with libexif
    seem to be getting a limited subset of what perl's exif reader reports.

---
+++ test/test_exif.js
@@ -0,0 +1,68 @@
+var should=require('should')
+var extract = require('../lib/extract_exif')
+
+describe('parse exif',function(){
+    it('should parse exif data from Nikon',function(done){
+        var file = 'test/files/DSC_8057.JPG'
+        extract(file,function(e,d){
+            should.not.exist(e)
+            should.exist(d)
+            d.should.have.property('Make')
+            d.should.have.property('Model')
+            d.Make.should.match(/nikon/i)
+            return done()
+        })
+    })
+
+    it('should parse exif data from Fujifilm',function(done){
+        var file = 'test/files/DSCF3084.JPG'
+        extract(file,function(e,d){
+            should.not.exist(e)
+            should.exist(d)
+            d.should.have.property('Make')
+            d.should.have.property('Model')
+            d.Make.should.match(/fujifilm/i)
+            return done()
+        })
+    })
+
+    it('should parse exif data from Samsung',function(done){
+        var file = 'test/files/SAM_0183.JPG'
+        extract(file,function(e,d){
+            should.not.exist(e)
+            should.exist(d)
+            d.should.have.property('Make')
+            d.should.have.property('Model')
+            d.Make.should.match(/samsung/i)
+            console.log(d)
+            return done()
+        })
+    })
+
+    it('should parse exif data from Sony',function(done){
+        var file = 'test/files/dsc00014.jpg'
+        extract(file,function(e,d){
+            should.not.exist(e)
+            should.exist(d)
+            d.should.have.property('Make')
+            d.should.have.property('Model')
+            d.Make.should.match(/sony/i)
+            console.log(d)
+            return done()
+        })
+    })
+
+    it('should parse exif data from Casio',function(done){
+        var file = 'test/files/cimg0330.jpg'
+        extract(file,function(e,d){
+            should.not.exist(e)
+            should.exist(d)
+            d.should.have.property('Make')
+            d.should.have.property('Model')
+            d.Make.should.match(/casio/i)
+            console.log(d)
+            return done()
+        })
+    })
+
+})
